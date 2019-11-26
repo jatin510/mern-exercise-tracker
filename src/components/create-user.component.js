434a3d8c1eedbb4d4bb30538d928fc61dbed4654
jatin510 <jatin6972@gmail.com>
2019-11-26 15:25:56 +0530

    problem with create user component

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export default class CreateUser extends Component {
   render() {
+    console.log("hello");
     return (
       <div>
         <p>You are on the Create Exercises List Component</p>
--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 
 export default class ExercisesList extends Component {
+  componentDidMount() {
+    console.log("exercise list mounted");
+  }
   render() {
+    console.log("hello");
     return (
       <div>
         <p>You are on the Exercises List Component</p>
